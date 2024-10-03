8a4bde714dff4ff84781739f980c6762db2c633e
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-10-03 14:16:07 -0700

    Updated the JWTAuthentication definition to use the updated Token model

--- users/authentication.py
+++ users/authentication.py
@@ -1,7 +1,8 @@
 from rest_framework.authentication import BaseAuthentication
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework.exceptions import AuthenticationFailed
-from users.models import User
+from mongoengine.errors import DoesNotExist
+from users.models import User, Token
 from bson import ObjectId
 
 class JWTAuthentication(BaseAuthentication):
@@ -13,8 +14,16 @@ class JWTAuthentication(BaseAuthentication):
         try:
             access_token = AccessToken(token.split(' ')[1])
             user_id = access_token['user_id']
+
             user = User.objects.get(id=ObjectId(user_id))
+
+            token_from_db = Token.objects.get(user=user)
+            print(f"Token from db: {token_from_db.token}")
+            print(f"Token from re: {token.split(' ')[1]}")
+            # The token passed in will be "Bearer <token>" where token is the access token
+            if token_from_db.token != token.split(' ')[1]:
+                raise AuthenticationFailed('Invalid token')
             user.authenticated = True
             return user, None
-        except (User.DoesNotExist, KeyError):
+        except (DoesNotExist, KeyError):
             raise AuthenticationFailed('Invalid token or user does not exist')
