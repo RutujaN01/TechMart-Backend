05b5abedbd0648377c450d56b866410842047b24
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-10-02 23:16:48 -0700

    Updated the admin_route decorator to use the JWT to determine if the user is an admin or not

--- decorators.py
+++ decorators.py
@@ -1,9 +1,8 @@
 from functools import wraps
 
-from bson import ObjectId
 from django.http import JsonResponse
-from django.contrib.auth import get_user_model
 from mongoengine import DoesNotExist
+from rest_framework_simplejwt.tokens import AccessToken
 
 from users.models import User
 
@@ -11,16 +10,21 @@ from users.models import User
 def admin_route(view_func):
     @wraps(view_func)
     def wrapper(request, *args, **kwargs):
-        # Validate and authenticate the user
-        print(request.headers)
-        user_id = request.headers.get('Requester-Id')
-        # user_id = ObjectId(user_id)
-        # print(user_id)
-        if not user_id:
-            return JsonResponse({"error": "Unauthorized: No user_id provided"}, status=401)
+        # Extract the JWT token from the Authorization header
+        token = request.headers.get('Authorization')
+        if not token:
+            return JsonResponse({"error": "Unauthorized: No token provided"}, status=401)
 
         try:
-            user = User.objects().get(id=user_id)
+            # Validate the JWT token and extract the user ID
+            access_token = AccessToken(token.split(' ')[1])
+            user_id = access_token['user_id']
+        except Exception as e:
+            return JsonResponse({"error": f"Unauthorized: Invalid token - {str(e)}"}, status=401)
+
+        try:
+            # Fetch the user from the database
+            user = User.objects.get(id=user_id)
         except DoesNotExist:
             return JsonResponse({"error": "Unauthorized: Invalid user_id"}, status=401)
 
