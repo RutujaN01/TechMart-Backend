02beefb4d074ca54dfdf11cfe773efdf037eb14b
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-10-09 13:59:27 -0700

    Formatted the files

--- wishlists/models.py
+++ wishlists/models.py
@@ -1,21 +1,22 @@
-from mongoengine import Document, StringField, IntField, FloatField, ListField, DecimalField, ReferenceField, CASCADE, \
+from mongoengine import Document, StringField, ListField, ReferenceField, CASCADE, \
     BooleanField
-from django.db import models
+
+from items.models import Items
 # https://docs.mongoengine.org/tutorial.html#defining-our-documents
 # 1.2.2.4. Handling deletions of references and 1.2.2.1. Posts
 from users.models import User
-from items.models import Items
+
+
 # Create your models here.
 class Wishlists(Document):
-    name = StringField(required = True, unique_with='user',  max_length = 50)
-    user = ReferenceField(User, unique_with='name', reverse_delete_rule= CASCADE)
-    items = ListField(ReferenceField(Items, reverse_delete_rule= CASCADE), default=list)
-    isPublic = BooleanField(default = True)
+    name = StringField(required=True, unique_with='user', max_length=50)
+    user = ReferenceField(User, unique_with='name', reverse_delete_rule=CASCADE)
+    items = ListField(ReferenceField(Items, reverse_delete_rule=CASCADE), default=list)
+    isPublic = BooleanField(default=True)
 
     def save(self, *args, **kwargs):
         super(Wishlists, self).save(*args, **kwargs)
 
-        
     meta = {
         'collection': 'wishlists'
     }
--- wishlists/serializers.py
+++ wishlists/serializers.py
@@ -4,6 +4,7 @@ from items.serializers import ItemsSerializer
 from users.serializers import UserSerializer
 from .models import Wishlists
 
+
 class WishlistsSerializer(DocumentSerializer):
     user = UserSerializer()
     items = ItemsSerializer(many=True)
