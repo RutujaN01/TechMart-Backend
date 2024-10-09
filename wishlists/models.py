977257d0e163dc60d36cfadbded6f49f9f2fab3e
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-10-09 12:19:10 -0700

    Added a field for managing if a wishlist is public or not

--- wishlists/models.py
+++ wishlists/models.py
@@ -1,4 +1,5 @@
-from mongoengine import Document, StringField, IntField, FloatField, ListField, DecimalField, ReferenceField, CASCADE
+from mongoengine import Document, StringField, IntField, FloatField, ListField, DecimalField, ReferenceField, CASCADE, \
+    BooleanField
 from django.db import models
 # https://docs.mongoengine.org/tutorial.html#defining-our-documents
 # 1.2.2.4. Handling deletions of references and 1.2.2.1. Posts
@@ -9,7 +10,8 @@ class Wishlists(Document):
     name = StringField(required = True, unique_with='user',  max_length = 30)
     user = ReferenceField(User, unique_with='name', reverse_delete_rule= CASCADE)
     items = ListField(ReferenceField(Items, reverse_delete_rule= CASCADE))
-    
+    isPublic = BooleanField(default = True)
+
     def save(self, *args, **kwargs):
         super(Wishlists, self).save(*args, **kwargs)
 
