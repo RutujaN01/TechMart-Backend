d495226ce130f1f3b787f7b949215f8749ca44fa
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-10-08 19:50:21 -0700

    Added a view for adding new wishlists

--- wishlists/views.py
+++ wishlists/views.py
@@ -10,3 +10,25 @@ from .models import Wishlists
 from .serializers import WishlistsSerializer
 
 # Create your views here.
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_wishlist(request):
+    # Extract the wishlist data from the request and create a new wishlist
+    wishlist_data = request.data
+    wishlist = Wishlists(**wishlist_data)
+    wishlist.user = request.user
+
+    # Save the wishlist to the database
+    try:
+        wishlist.save()
+    except NotUniqueError as e:
+        # If the wishlist already exists, return an error
+        return Response({"error": "Wishlist already exists with that information"}, status=400)
+    except Exception as e:
+        # If there is an error saving the wishlist, return the
+        return Response({"error": str(e)}, status=400)
+
+    # Serialize the wishlist data and return it
+    wishlist_data = WishlistsSerializer(wishlist).data
+    return Response({"data": wishlist_data})
+
