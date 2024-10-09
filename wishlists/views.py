9f55a5814e401ce144462f7a1d3d394f0c9596aa
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-10-08 18:35:59 -0700

    Set up imports

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from mongoengine.errors import NotUniqueError
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from items.models import Items
+from items.serializers import ItemsSerializer
+from users.models import User
+from .models import Wishlists
+from .serializers import WishlistsSerializer
 
 # Create your views here.
