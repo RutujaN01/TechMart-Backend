2838945181a4e71f28acb1c823288993e707df02
Zackary Liel <47030040+zliel@users.noreply.github.com>
2024-10-12 22:59:53 -0700

    Added a view for users to update themselves and for admins to update a user

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.urls import path
 
-from .views import get_users, create_user, login, get_current_user, logout, delete_user
+from .views import get_users, create_user, login, get_current_user, logout, delete_user, update_user
 
 urlpatterns = [
     path('', get_users, name='Get All Users'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('logout/', logout, name='Logout'),
     path('delete/user', delete_user, name='Delete User'),
     path('get_current_user/', get_current_user, name='Get Current User'),
+    path('update', update_user, name='Update User'),
 ]
